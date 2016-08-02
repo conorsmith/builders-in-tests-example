00685dbda56e6ca0349d263a6945a8bd076ac768
Conor Smith <conorjbsmith@gmail.com>
2016-08-02 09:22:34 +0100

    Unit test case with a builder

--- tests/TrackTest.php
+++ tests/TrackTest.php
@@ -13,7 +13,9 @@ class TrackTest extends \PHPUnit_Framework_TestCase
      */
     public function it_can_be_queried_for_its_name()
     {
-        $track = Track::fromNameAndDuration("the track's name", Duration::fromSeconds(198));
+        $track = $this->track()
+            ->withName("the track's name")
+            ->build();
 
         $this->assertSame("the track's name", $track->getName());
     }
@@ -23,8 +25,45 @@ class TrackTest extends \PHPUnit_Framework_TestCase
      */
     public function it_can_be_queried_for_its_duration()
     {
-        $track = Track::fromNameAndDuration("the track's name", Duration::fromSeconds(198));
+        $track = $this->track()
+            ->withDuration(Duration::fromSeconds(198))
+            ->build();
 
         $this->assertTrue(Duration::fromSeconds(198)->equals($track->getDuration()));
+    }
+
+    private function track()
+    {
+        return new class()
+        {
+            /** @var string */
+            private $name;
+
+            /** @var Duration */
+            private $duration;
+
+            public function __construct()
+            {
+                $this->name = "some name";
+                $this->duration = Duration::fromSeconds(123);
+            }
+
+            public function withName(string $name): self
+            {
+                $this->name = $name;
+                return $this;
+            }
+
+            public function withDuration(Duration $duration): self
+            {
+                $this->duration = $duration;
+                return $this;
+            }
+
+            public function build(): Track
+            {
+                return Track::fromNameAndDuration($this->name, $this->duration);
+            }
+        };
     }
 }
