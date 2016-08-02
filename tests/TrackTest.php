8d2d58c9c2200328520d3ab33eefc394da656dda
Conor Smith <conorjbsmith@gmail.com>
2016-08-02 09:25:12 +0100

    Unit test case with multiple builders

--- tests/TrackTest.php
+++ tests/TrackTest.php
@@ -26,15 +26,15 @@ class TrackTest extends \PHPUnit_Framework_TestCase
     public function it_can_be_queried_for_its_duration()
     {
         $track = $this->track()
-            ->withDuration(Duration::fromSeconds(198))
+            ->withDuration($duration = $this->duration()->build())
             ->build();
 
-        $this->assertTrue(Duration::fromSeconds(198)->equals($track->getDuration()));
+        $this->assertTrue($duration->equals($track->getDuration()));
     }
 
     private function track()
     {
-        return new class()
+        return new class($this->duration()->build())
         {
             /** @var string */
             private $name;
@@ -42,10 +42,10 @@ class TrackTest extends \PHPUnit_Framework_TestCase
             /** @var Duration */
             private $duration;
 
-            public function __construct()
+            public function __construct(Duration $defaultDuration)
             {
                 $this->name = "some name";
-                $this->duration = Duration::fromSeconds(123);
+                $this->duration = $defaultDuration;
             }
 
             public function withName(string $name): self
@@ -63,6 +63,31 @@ class TrackTest extends \PHPUnit_Framework_TestCase
             public function build(): Track
             {
                 return Track::fromNameAndDuration($this->name, $this->duration);
+            }
+        };
+    }
+
+    private function duration()
+    {
+        return new class()
+        {
+            /** @var int */
+            private $seconds;
+
+            public function __construct()
+            {
+                $this->seconds = 123;
+            }
+
+            public function withSeconds(int $seconds): self
+            {
+                $this->seconds = $seconds;
+                return $this;
+            }
+
+            public function build(): Duration
+            {
+                return Duration::fromSeconds($this->seconds);
             }
         };
     }
